787fdafa1266fa30f37e6fc3e74592abf5aae360
JinTao <jza237@sfu.ca>
2021-07-17 01:38:01 -0700

    menu buttons now direct onto other pages

--- GetFitOrGetHit/app/src/main/java/com/example/getfitorgethit/MainActivity.kt
+++ GetFitOrGetHit/app/src/main/java/com/example/getfitorgethit/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.example.getfitorgethit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        //statspage.setonclick
+
+        var statsbutton = findViewById<Button>(R.id.statspagebutton)
+        statsbutton.setOnClickListener {
+            val intentstats = Intent(this, StatsPage::class.java)
+            startActivity(intentstats)
+        }
+
+        var questsbutton = findViewById<Button>(R.id.questpagebutton)
+        questsbutton.setOnClickListener {
+            val intentquests = Intent(this, QuestsPage::class.java)
+            startActivity(intentquests)
+        }
+
+        var shopbutton = findViewById<Button>(R.id.shoppagebutton)
+        shopbutton.setOnClickListener {
+            val intentshop = Intent(this, ShopPage::class.java)
+            startActivity(intentshop)
+        }
+
+        var settingsbutton = findViewById<Button>(R.id.settingspagebutton)
+        settingsbutton.setOnClickListener {
+            val intentsettings = Intent(this, SettingsPage::class.java)
+            startActivity(intentsettings)
+        }
     }
 }
---
+++ GetFitOrGetHit/app/src/main/java/com/example/getfitorgethit/StatsPage.java
@@ -0,0 +1,14 @@
+package com.example.getfitorgethit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats_page);
+    }
+}
