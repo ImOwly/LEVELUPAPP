99a4b892aa4327b285785a00641faed92c6c1084
MaxiKolbe <randyletran@gmail.com>
2021-07-17 15:46:41 -0700

    Character Page Changes

---
+++ GetFitOrGetHit/app/src/main/java/com/example/getfitorgethit/Stat_Character_View.kt
@@ -0,0 +1,11 @@
+package com.example.getfitorgethit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Stat_Character_View : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stat_character_view)
+    }
+}
